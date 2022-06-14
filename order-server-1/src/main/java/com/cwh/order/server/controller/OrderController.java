6874bbe0cbece5d4138f208fe19837cff9593be7
caiwenhao <caiwenhao@richstonedt.com>
2022-06-14 17:59:17 +0800

    添加订单，客户服务

---
+++ order-server-1/src/main/java/com/cwh/order/server/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.cwh.order.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+    @GetMapping("/getOrder")
+    public String getOrder() {
+        return "this is order server";
+    }
+
+}
---
+++ user-server-1/src/main/java/com/cwh/user/server/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.cwh.user.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("getUser")
+    public String getUser() {
+        //调用订单服务
+        String orderServerUrl = "http://orderServer-1/order/getOrder";
+        String orderResult = restTemplate.getForObject(orderServerUrl, String.class);
+        System.out.println(orderResult);
+        return "this is user server";
+    }
+
+}
