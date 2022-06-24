e48949aad06a09222df5de565ea51130cfd79adb
caiwenhao <caiwenhao@richstonedt.com>
2022-06-24 18:17:29 +0800

    引入Gateway依赖

---
+++ gateway-server/src/main/java/com/cwh/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.cwh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b><code>GatewayApplication</code></b>
+ * <p/>
+ * Description
+ * <p/>
+ * <b>Creation Time:</b> 2022/6/24 下午6:11.
+ *
+ * @author caiwenhao
+ * @since spring-cloud-demo-v2
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
